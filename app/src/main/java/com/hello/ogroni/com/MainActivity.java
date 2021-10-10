aef8f58b2061ca10a3033b883cc6a680983e43fc
jamil-sagar <jamil.sagar@ogroni.net>
2021-10-10 15:40:28 +0600

    App icon, number and text fixed

---
+++ app/src/main/java/com/hello/ogroni/com/MainActivity.java
@@ -0,0 +1,106 @@
+package com.hello.ogroni.com;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.hello.ogroni.com.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    List<String> list;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        list = new ArrayList<>();
+        list.add("First position");
+        list.add("Second position");
+        list.add("Third position");
+        list.add("Fourth position");
+        list.add("Fifth position");
+        list.add("Sixth position");
+        list.add("Seventh position");
+        list.add("Eighth position");
+        list.add("Ninth position");
+        list.add("Tenth position");
+        list.add("Eleventh position");
+        list.add("Twelfth position");
+    }
+
+    public void runOne(View view) {
+        binding.middleNumber.setText("1");
+        binding.footerTxt.setText(list.get(0));
+    }
+
+    public void runTwo(View view) {
+        binding.middleNumber.setText("2");
+        binding.footerTxt.setText(list.get(1));
+    }
+
+    public void runThree(View view) {
+        binding.middleNumber.setText("3");
+        binding.footerTxt.setText(list.get(2));
+    }
+
+    public void runFour(View view) {
+        binding.middleNumber.setText("4");
+        binding.footerTxt.setText(list.get(3));
+    }
+
+    public void runFive(View view) {
+        String fivePos = binding.fivePositionTxt.getText().toString();
+        binding.middleNumber.setText(fivePos);
+        binding.footerTxt.setText(list.get(4));
+    }
+
+    public void runSix(View view) {
+        String sixPos = binding.sixPositionTxt.getText().toString();
+        binding.middleNumber.setText(sixPos);
+        binding.footerTxt.setText(list.get(5));
+    }
+
+    public void runSeven(View view){
+        String sevenPos = binding.sevenPositionTxt.getText().toString();
+        binding.middleNumber.setText(sevenPos);
+        binding.footerTxt.setText(list.get(6));
+    }
+
+    public void runEight(View view){
+        String eightPos = binding.eightPositionTxt.getText().toString();
+        binding.middleNumber.setText(eightPos);
+        binding.footerTxt.setText(list.get(7));
+    }
+
+    public void runNine(View view){
+        String ninePos = binding.ninePositionTxt.getText().toString();
+        binding.middleNumber.setText(ninePos);
+        binding.footerTxt.setText(list.get(8));
+    }
+
+    public void runTen(View view){
+        String tenPos = binding.tenPositionTxt.getText().toString();
+        binding.middleNumber.setText(tenPos);
+        binding.footerTxt.setText(list.get(9));
+    }
+
+    public void runEleven(View view){
+        String elevenPos = binding.elevenPositionTxt.getText().toString();
+        binding.middleNumber.setText(elevenPos);
+        binding.footerTxt.setText(list.get(10));
+    }
+
+    public void runTwelve(View view){
+        String twelvePos = binding.twelvePositionTxt.getText().toString();
+        binding.middleNumber.setText(twelvePos);
+        binding.footerTxt.setText(list.get(11));
+    }
+}
